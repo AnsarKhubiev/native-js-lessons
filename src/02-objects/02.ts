c665328a3806e152c3a93cc7e374c9901da02e4e
AnsarKhubiev <anskhubiev@icloud.com>
2024-03-01 16:02:59 +0300

    lesson-02 objects

---
+++ src/02-objects/02.ts
@@ -0,0 +1,62 @@
+
+type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+type TechType = {
+    id: number
+    title: string
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+const student: StudentType = {
+    id: 1,
+    "name": 'Ansar',
+    age: 34,
+    isActive: false,
+    address: {
+        streetTitle: "Mutlu City",
+
+        city: {
+            title: "Istanbul",
+            countryTitle: 'Turkey'
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "JS"
+        },
+        {
+            id: 3,
+            title: "CSS"
+        },
+        {
+            id: 4,
+            title: "React"
+        },
+    ]
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
